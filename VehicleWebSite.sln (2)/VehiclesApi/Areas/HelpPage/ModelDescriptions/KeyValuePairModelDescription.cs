9e3614f9b99d16c146f735d36c877c0b86f6061f
vineethyalaka <vy34@njit.edu>
2018-09-18 18:36:20 -0400

    Updated
    Added files for Webservices and For Api

--- VehicleWebSite.sln (2)/VehiclesApi/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
+++
@@ -1,9 +0,0 @@
-namespace VehiclesApi.Areas.HelpPage.ModelDescriptions
-{
-    public class KeyValuePairModelDescription : ModelDescription
-    {
-        public ModelDescription KeyModelDescription { get; set; }
-
-        public ModelDescription ValueModelDescription { get; set; }
-    }
-}
